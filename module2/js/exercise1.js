ea2986bfb53a6012f286893ca85eba46fad754c7
Joan Viray <virayjoan1985@gmail.com>
2024-04-28 07:29:06 +0800

    initial commit

---
+++ module2/js/exercise1.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "mango", "banana"];    
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+console.log(fruits.length);
+
+fruits[1] = "kiwi";
+console.log(fruits[1]);
